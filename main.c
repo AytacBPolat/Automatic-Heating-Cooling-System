c0a2b9df8c167f6769bb8c52f1bb95b263ccdd7e
AytacBPolat <aytacbaranpolat@gmail.com>
2025-05-06 20:34:41 +0300

    7-segment implementation

--- main.c
+++ main.c
@@ -1,13 +1,26 @@
 #include <msp430.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
 
-#define HEATER_PIN    BIT0    // P1.0
-#define FAN_PIN       BIT1    // P1.1
-#define TARGET_TEMP   28.0f   // Hem ısıtma/soğutma eşik değeri
+//TM1637 Pins
+#define CLK_PIN     BIT6
+#define DIO_PIN     BIT7
+#define CLK_DIR     P1DIR
+#define DIO_DIR     P1DIR
+#define CLK_OUT     P1OUT
+#define DIO_OUT     P1OUT
+#define DIO_IN      P1IN
+
+//Heating and Cooling Pins
+#define HEATER_PIN    BIT0
+#define FAN_PIN       BIT1
+#define TARGET_TEMP   30.0f
 #define HYSTERESIS    2.0f
+
 typedef enum { HEATING_STATE, COOLING_STATE } SystemState;
 static SystemState current_state;
 
-// Donanımı bir kere başlat
 void setup() {
     WDTCTL = WDTPW | WDTHOLD;
     P1DIR |= HEATER_PIN | FAN_PIN;
@@ -15,9 +28,17 @@ void setup() {
     ADC10CTL0 = ADC10SHT_3 | ADC10ON;     // 64CLK sample, ADC on
     ADC10AE0 |= BIT4;                     // P1.4 analog
     ADC10CTL0 |= ENC;
+
+    BCSCTL1 = CALBC1_1MHZ;
+    DCOCTL = CALDCO_1MHZ;
+    CLK_DIR |= CLK_PIN;
+    DIO_DIR |= DIO_PIN;
+
+    // Start state: Both Pins High
+    CLK_OUT |= CLK_PIN;
+    DIO_OUT |= DIO_PIN;
 }
 
-// Son 5 ölçümün medyanını döndürür
 float read_temp() {
     static float buf[5];
     static unsigned char idx = 0;
@@ -29,8 +50,6 @@ float read_temp() {
 
     buf[idx] = ((float)ADC10MEM * 3.53f / 1023.0f) / 0.01f;
     idx = (idx + 1) % 5;
-
-    // Küçükten büyüğe sıralayıp ortadaki(2.) elemanı al
     for (i = 0; i < 5; i++) s[i] = buf[i];
     for (i = 0; i < 4; i++) {
         for (j = i+1; j < 5; j++) {
@@ -42,7 +61,6 @@ float read_temp() {
     return s[2];
 }
 
-// Mevcut state'e göre pinleri kesin olarak ayarlar
 void apply_state(SystemState st) {
     if (st == HEATING_STATE) {
         P1OUT |=  HEATER_PIN;
@@ -53,10 +71,9 @@ void apply_state(SystemState st) {
     }
 }
 
-// Sıcaklığa göre anında geçiş
-void control_system() {
+int control_system() {
     float temp = read_temp();
-    //feature branch değişiklikleri
+
     if (current_state == HEATING_STATE) {
         if (temp >= TARGET_TEMP + HYSTERESIS) {
             current_state = COOLING_STATE;
@@ -68,14 +85,120 @@ void control_system() {
             apply_state(current_state);
         }
     }
+
+    return (int)temp;
 }
 
-int main() {
-    unsigned char i;
+void TM1637_delay() {
+    __delay_cycles(100); //100us
+}
 
-    setup();
+void TM1637_setDIOInput() {
+    DIO_DIR &= ~DIO_PIN;
+}
 
-    // Filtre dolsun diye birkaç ölçüm alıyoruz
+
+void TM1637_setDIOOutput() {
+    DIO_DIR |= DIO_PIN;
+}
+
+
+void TM1637_start() {
+    TM1637_setDIOOutput();
+    CLK_OUT |= CLK_PIN;
+    DIO_OUT |= DIO_PIN;
+    TM1637_delay();
+    DIO_OUT &= ~DIO_PIN;
+    TM1637_delay();
+    CLK_OUT &= ~CLK_PIN;
+}
+
+
+void TM1637_stop() {
+    TM1637_setDIOOutput();
+    CLK_OUT &= ~CLK_PIN;
+    DIO_OUT &= ~DIO_PIN;
+    TM1637_delay();
+    CLK_OUT |= CLK_PIN;
+    TM1637_delay();
+    DIO_OUT |= DIO_PIN;
+}
+
+
+void TM1637_writeByte(uint8_t b) {
+    TM1637_setDIOOutput();
+    int i;
+    for ( i = 0; i < 8; i++) {
+        CLK_OUT &= ~CLK_PIN;
+        if (b & 0x01) {
+            DIO_OUT |= DIO_PIN;
+        } else {
+            DIO_OUT &= ~DIO_PIN;
+        }
+        TM1637_delay();
+        CLK_OUT |= CLK_PIN;
+        TM1637_delay();
+        b >>= 1;
+    }
+
+
+    CLK_OUT &= ~CLK_PIN;
+    TM1637_setDIOInput();
+    TM1637_delay();
+    CLK_OUT |= CLK_PIN;
+    TM1637_delay();
+
+    CLK_OUT &= ~CLK_PIN;
+    TM1637_setDIOOutput();
+}
+
+
+void TM1637_sendCommand(uint8_t cmd) {
+    TM1637_start();
+    TM1637_writeByte(cmd);
+    TM1637_stop();
+}
+
+
+void TM1637_displayByte(uint8_t addr, uint8_t data) {
+    TM1637_start();
+    TM1637_writeByte(0xC0 | addr);
+    TM1637_writeByte(data);
+    TM1637_stop();
+}
+
+
+const uint8_t segData[] = {
+    0x3F, // 0
+    0x06, // 1
+    0x5B, // 2
+    0x4F, // 3
+    0x66, // 4
+    0x6D, // 5
+    0x7D, // 6
+    0x07, // 7
+    0x7F, // 8
+    0x6F  // 9
+};
+
+
+void TM1637_displayNumber(int num) {
+    TM1637_sendCommand(0x40);
+
+    TM1637_start();
+    TM1637_writeByte(0xC0);
+    TM1637_writeByte(segData[(num / 1000) % 10]);
+    TM1637_writeByte(segData[(num / 100) % 10]);
+    TM1637_writeByte(segData[(num / 10) % 10]);
+    TM1637_writeByte(segData[num % 10]);
+    TM1637_stop();
+
+    TM1637_sendCommand(0x88 | 0x07); // brightness = max (0x88 to 0x8F)
+}
+
+void main(void) {
+   setup();
+   int i;
     for (i = 0; i < 5; i++) {
         read_temp();
         __delay_cycles(800000); //8MHZ timer için 0.1 sn
@@ -88,9 +211,9 @@ int main() {
         current_state = HEATING_STATE;
     apply_state(current_state);
 
-    // Ana döngü
     while (1) {
-        control_system();
-        __delay_cycles(8000000);
+        int temperature = control_system();
+        TM1637_displayNumber(temperature);
+        __delay_cycles(1000000); //wait for 1 sec.
     }
 }
